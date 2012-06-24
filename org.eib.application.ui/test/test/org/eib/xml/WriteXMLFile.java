ccda53144d149258df8bb5a8e198e583638f4326
tuanemtv <tuanemtv@a1609d13-3ef6-a8f8-5e0b-472c95e60613>
2012-06-24 15:44:40 +0000

    Ghi class query xuống file xml

--- org.eib.application.ui/test/test/org/eib/xml/WriteXMLFile.java
+++ org.eib.application.ui/test/test/org/eib/xml/WriteXMLFile.java
@@ -1,6 +1,8 @@
 package test.org.eib.xml;
 
 import java.io.File;
+import java.io.IOException;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -10,113 +12,125 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
  
+import org.apache.log4j.Logger;
+import org.eib.database.Query;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
  
 public class WriteXMLFile {
- 
+	private static Query[] _query;
+	private static Logger logger =Logger.getLogger("WriteXMLFile");
+	
 	public static void main(String argv[]) {
  
-	  try {
- 
-		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
- 
-		// root elements
-		Document doc = docBuilder.newDocument();
-		Element rootElement = doc.createElement("ScriptCommon");
-		doc.appendChild(rootElement);
-
-		Attr attr = doc.createAttribute("id");
-		attr.setValue("All Script	");
-		rootElement.setAttributeNode(attr);
 		
-		// staff elements
-		Element staff = doc.createElement("Query");
-		rootElement.appendChild(staff);
- 
-		// set attribute to staff element
-		attr = doc.createAttribute("id");
-		attr.setValue("LN01");
-		staff.setAttributeNode(attr);
- 
-		// shorten way
-		// staff.setAttribute("id", "1");
- 
+		Query qur = new Query();//luc khoi dau lay duoc gia tri ko?
+		_query = new Query[123];
+		
+		
+		try {
+			qur.getXMLToScript("D:\\Query to Excel\\Congifure\\script.xml", "Query", _query);
+			
+			try {
+				 
+				DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		 
+				// root elements
+				Document doc = docBuilder.newDocument();
+				Element rootElement = doc.createElement("ScriptCommon");
+				doc.appendChild(rootElement);
 	
-		Element firstname = doc.createElement("querynm");
-		firstname.appendChild(doc.createTextNode("LN - 01. Khe uoc thay doi lai suat theo chu ky (ca nhan)"));
-		staff.appendChild(firstname);
- 
-		// lastname elements
-		Element lastname = doc.createElement("status");
-		lastname.appendChild(doc.createTextNode("0"));
-		staff.appendChild(lastname);
- 
-		// nickname elements
-		Element nickname = doc.createElement("server");
-		nickname.appendChild(doc.createTextNode("Oralce-AReport"));
-		staff.appendChild(nickname);
- 
-		// salary elements
-		Element salary = doc.createElement("module");
-		salary.appendChild(doc.createTextNode("LN"));
-		staff.appendChild(salary);
- 
-		
-		
+				Attr attr = doc.createAttribute("id");
+				attr.setValue("All Script	");
+				rootElement.setAttributeNode(attr);
+				
+				Element eleTemp;
+				
+				for (int i=0; i< _query.length; i++){
+					logger.info("["+(i+1)+"] queryid: " + _query[i].get_queryid()+", name: "+_query[i].get_querynm());
+					_query[i].setquery();
+						
+					// staff elements
+					Element staff = doc.createElement("Query");
+					rootElement.appendChild(staff);
+			 
+					// set attribute to staff element
+					attr = doc.createAttribute("id");
+					attr.setValue(_query[i].get_queryid());
+					staff.setAttributeNode(attr);
+			 
+					// shorten way
+					// staff.setAttribute("id", "1");
+				
+					eleTemp = doc.createElement("queryid");
+					eleTemp.appendChild(doc.createTextNode(_query[i].get_queryid()));
+					staff.appendChild(eleTemp);
+			 
+					
+					eleTemp = doc.createElement("querynm");
+					eleTemp.appendChild(doc.createTextNode(_query[i].get_querynm()));
+					staff.appendChild(eleTemp);
+			 
+					eleTemp = doc.createElement("status");
+					eleTemp.appendChild(doc.createTextNode(_query[i].get_status()));
+					staff.appendChild(eleTemp);
+					
+					eleTemp = doc.createElement("module");
+					eleTemp.appendChild(doc.createTextNode(_query[i].get_module()));
+					staff.appendChild(eleTemp);
+					/*
+					eleTemp = doc.createElement("fileurl");
+					eleTemp.appendChild(doc.createTextNode(_query[i].get_fileurl()));
+					staff.appendChild(eleTemp);*/
+					/*
+					eleTemp = doc.createElement("description");
+					eleTemp.appendChild(doc.createTextNode(_query[i].get_description()));
+					staff.appendChild(eleTemp);
+					
+					eleTemp = doc.createElement("note");
+					eleTemp.appendChild(doc.createTextNode(_query[i].get_note()));
+					staff.appendChild(eleTemp);
+					*/
+					
+				}
+				// write the content into xml file
+				TransformerFactory transformerFactory = TransformerFactory.newInstance();
+				Transformer transformer = transformerFactory.newTransformer();
+				DOMSource source = new DOMSource(doc);
+				StreamResult result = new StreamResult(new File("D:\\test.xml"));
+		 
+				// Output to console for testing
+				// StreamResult result = new StreamResult(System.out);
+		 
+				transformer.transform(source, result);
+		 
+				System.out.println("File saved!");
+		 
+			  } catch (ParserConfigurationException pce) {
+				pce.printStackTrace();
+			  } catch (TransformerException tfe) {
+				tfe.printStackTrace();
+			  }	
+			//System.out.println("> Load script Done. With= "+_query.length+" scripts");
+		} catch (ParserConfigurationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			return;	
+		} catch (SAXException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			return;	
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
 
-		// staff elements
-		staff = doc.createElement("Query");
-		rootElement.appendChild(staff);
- 
-		// set attribute to staff element
-		attr = doc.createAttribute("id");
-		attr.setValue("LN02");
-		staff.setAttributeNode(attr);
- 
-		// shorten way
-		// staff.setAttribute("id", "1");
- 
-	
-		firstname = doc.createElement("querynm");
-		firstname.appendChild(doc.createTextNode("LN - 01. Khe uoc thay doi lai suat theo chu ky (ca nhan)"));
-		staff.appendChild(firstname);
- 
-		// lastname elements
-		lastname = doc.createElement("status");
-		lastname.appendChild(doc.createTextNode("0"));
-		staff.appendChild(lastname);
- 
-		// nickname elements
-		nickname = doc.createElement("server");
-		nickname.appendChild(doc.createTextNode("Oralce-AReport"));
-		staff.appendChild(nickname);
- 
-		// salary elements
-		salary = doc.createElement("module");
-		salary.appendChild(doc.createTextNode("LN"));
-		staff.appendChild(salary);
- 
+			e1.printStackTrace();
+			return;	
+		}
 		
-		// write the content into xml file
-		TransformerFactory transformerFactory = TransformerFactory.newInstance();
-		Transformer transformer = transformerFactory.newTransformer();
-		DOMSource source = new DOMSource(doc);
-		StreamResult result = new StreamResult(new File("D:\\test.xml"));
- 
-		// Output to console for testing
-		// StreamResult result = new StreamResult(System.out);
- 
-		transformer.transform(source, result);
- 
-		System.out.println("File saved!");
- 
-	  } catch (ParserConfigurationException pce) {
-		pce.printStackTrace();
-	  } catch (TransformerException tfe) {
-		tfe.printStackTrace();
-	  }
+		
+	 
 	}
 }
