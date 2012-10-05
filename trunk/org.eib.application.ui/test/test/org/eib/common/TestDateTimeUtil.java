b041fc28290c25f71237067c366047e12f198d28
tuanemtv@gmail.com <tuanemtv@gmail.com@a1609d13-3ef6-a8f8-5e0b-472c95e60613>
2012-10-05 16:17:45 +0000

    Them tinh nang chay tu dong

---
+++ trunk/org.eib.application.ui/test/test/org/eib/common/TestDateTimeUtil.java
@@ -0,0 +1,15 @@
+package test.org.eib.common;
+
+import org.eib.common.DateTimeUtil;
+
+public class TestDateTimeUtil {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Date = "+DateTimeUtil.getDateYYYYMMDD());
+	}
+
+}
--- trunk/org.eib.application.ui/test/test/org/eib/io/GetFileList.java
+++ trunk/org.eib.application.ui/test/test/org/eib/io/GetFileList.java
@@ -13,7 +13,7 @@ public class GetFileList {
 	public static void main(String args[]){
 		 //File file = new File("D:\\20090610_SVN Client\\Local Project\\Script\\DP\\");  
 		 //File file = new File("E:\\BACKUP\\DROPBOX\\Dropbox\\WORK\\Project\\File Configure\\Tong script\\Tong script\\DP\\"); //LN  
-		 File file = new File("D:\\FTP Server Url\\dp\\src\\dpsa\\");
+		 File file = new File("N:\\ENGLISH LIST\\TEXTBOOK\\E - English for Office - Business Conversations\\");
 			//File file = new File("D:\\Query to Excel\\Total Result\\20120621\\"); //LN
 			//Long lastModified = file.lastModified();
 			 //Date date = new Date(lastModified);
@@ -25,6 +25,7 @@ public class GetFileList {
 		 for (int fileInList = 0; fileInList < files.length; fileInList++)  
 		 {  
 			 
+			 /*
 			 Date nowDate = new Date();
 			 DateFormat nowDateFormat = new SimpleDateFormat("yyyyMMdd");
 			 //System.out.println(nowDateFormat.format(nowDate));
@@ -38,9 +39,9 @@ public class GetFileList {
 			 if (dateFormat.format(date).equals(nowDateFormat.format(nowDate)))
 				 System.out.println(dateFormat.format(date)+" "+files[fileInList].toString());
 			 
-			
+			*/
 			 
-			 //System.out.println(files[fileInList].toString() + "thuoc tinh: ");  
+			 System.out.println(files[fileInList].toString());  
 			 
 		 }  
 	}
