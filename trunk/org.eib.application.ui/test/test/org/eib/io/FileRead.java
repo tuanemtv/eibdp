302234daa5403d6cbaa1b1287ed0d4d3610be4f6
tuanemtv@gmail.com <tuanemtv@gmail.com@a1609d13-3ef6-a8f8-5e0b-472c95e60613>
2012-11-01 15:06:04 +0000

    Change Query struct

--- trunk/org.eib.application.ui/test/test/org/eib/io/FileRead.java
+++ trunk/org.eib.application.ui/test/test/org/eib/io/FileRead.java
@@ -3,6 +3,8 @@ package test.org.eib.io;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class FileRead {
@@ -16,8 +18,10 @@ public class FileRead {
 	  // Open the file that is the first 
 	  // command line parameter
 	  //FileInputStream fstream = new FileInputStream("E:\\BACKUP\\DROPBOX\\Dropbox\\WORK\\Project\\script1.sql");
-	  FileInputStream fstream = new FileInputStream("D:\\20090610_SVN Client\\Local Project\\Script\\060 - PRT - Sac he Eximbank.sql");
+	  FileInputStream fstream = new FileInputStream("D:\\Project\\Report to Excel\\Workplace\\Report to Excel\\GG  Report to Excel\\Script\\MySQL\\testNumber.sql");
 	  // Get the object of DataInputStream
+	  
+	  /*
 	  DataInputStream in = new DataInputStream(fstream);
 	  BufferedReader br = new BufferedReader(new InputStreamReader(in));
 	  String strLine;
@@ -34,7 +38,28 @@ public class FileRead {
 	  in.close();
 	  System.out.println ("strGet  "+strGet);
 	  System.out.println ("strGet2  "+strGet2);
-	  
+	  */
+	  //FileInputStream fstream;
+		try {			
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			while ((strLine = br.readLine()) != null)   {	
+				//Neu bat dau bang select thi moi them vao.
+				//this.set_getquery(this.get_getquery() + strLine+'\n');//Cong them 1 khoang trang de cau script dung	  
+				 System.out.println (strLine);
+			}			 
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			//logger.error(e.getMessage());
+			e.printStackTrace();
+		}
+		 // Get the object of DataInputStream
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			//logger.error(e.getMessage());
+			e.printStackTrace();
+		}
 	    }catch (Exception e){//Catch exception if any
 	  System.err.println("Error: " + e.getMessage());
 	  }
