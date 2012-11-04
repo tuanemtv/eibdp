f152690a76560b932d51793b01578187004a25a0
tuanemtv <tuanemtv@gmail.com>
2012-11-04 02:55:46 +0000

    Create Query To Excel Cron

---
+++ org.eib.application.ui/Query To Excel Cron/src/org/eib/cron/application/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package org.eib.cron.application;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "Query_To_Excel_Cron.perspective"; //$NON-NLS-1$
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
