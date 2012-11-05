0b673b5a53431bba263cfb2edddfc1a6e4ff7722
tuanemtv.project <tuanemtv.project@gmail.com>
2012-11-05 16:33:22 +0000

    Cron run multi Thread

--- org.eib.application.ui/test/test/org/eib/String/TestString1.java
+++ org.eib.application.ui/test/test/org/eib/String/TestString1.java
@@ -10,8 +10,17 @@ public class TestString1 {
 		String str1 = "Cron02.S3";
 		String str2 = "S3";
 		
+		String str3 = "Cron02";
+		
 		if (str1.equals("Cron02."+str2)){
 			System.out.println("Bang nhau");
+		}
+		
+		System.out.println(str1.substring(0,6));
+		System.out.println(str1.substring(7));
+		
+		if (str3.equals(str1.substring(0,6))){
+			System.out.println("Bang nhau 2");
 		}
 	}
 
