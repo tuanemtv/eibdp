830ccbf5454ffd7a085d1f6a13cd81d55fe087e0
tuanemtv.project@gmail.com <tuanemtv.project@gmail.com@a1609d13-3ef6-a8f8-5e0b-472c95e60613>
2012-11-07 16:52:13 +0000

    Build thành công

--- trunk/Query To Excel Cron/src/org/eib/cron/run/QueryJob.java
+++ trunk/Query To Excel Cron/src/org/eib/cron/run/QueryJob.java
@@ -3,6 +3,7 @@ package org.eib.cron.run;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.ResourceBundle;
 
 import org.apache.log4j.Logger;
 import org.eib.common.FolderUtil;
@@ -19,17 +20,22 @@ public class QueryJob implements Job{
 	
 	private static Logger logger =Logger.getLogger("QueryJob");	
 	
+	
 	public void execute(JobExecutionContext context)
 			throws JobExecutionException {
 		
 		JobDataMap data = context.getJobDetail().getJobDataMap();		
-		logger.info("_cronNM= " + data.getString("_cronNM"));
+		logger.info("_cronNM= " + data.getString("_cronNM"));	
+		logger.info("_defineScript= " + data.getString("_defineScript"));
+		logger.info("_databaseID= " + data.getString("_databaseID"));
 		
-		//System.out.println("Run _cronNM= "+ data.getString("_cronNM"));
-		
-		MainCommon main = new MainCommon("/resource/app","1");
-		//main.get_querycron()[0].logQueryCron();
-		//main.get_querycron()[1].logQueryCron();
+		//System.out.println("Run-----------------");
+		//System.out.println("_cronNM= " + data.getString("_cronNM"));
+		//System.out.println("_defineScript= " + data.getString("_defineScript"));
+		//System.out.println("_databaseID= " + data.getString("_databaseID"));
+				
+		ResourceBundle rb = ResourceBundle.getBundle("/resource/app");				
+		MainCommon main = new MainCommon("/resource/app",rb.getString("app_kind"));
 		
 		Date date1 ;
 		DateFormat dateFormat1;
@@ -45,21 +51,23 @@ public class QueryJob implements Job{
 		date1= new Date();
 		main.get_appcommon().set_outurl(main.get_appcommon().get_outurl()+dateFormat2.format(date1)+"\\");				
 		//Tao folder
-		FolderUtil.createFolder(main.get_appcommon().get_outurl());
-				
+		FolderUtil.createFolder(main.get_appcommon().get_outurl());				
+							
 		
-		//Tim cron dung
-		//QueryCron _quecron = new QueryCron();
-		//_quecron = main.getQueryCronFromName(data.getString("_cronNM"));
-		//_quecron.logQueryCron();
-		
-		
-		String [] a= null;
-		a = main.getQueryIDFromName(data.getString("_cronNM"));
-		
-
-		Query[] _arrQueryTemp = new Query[a.length];
-		
+		QueryServer _qurser = new QueryServer();
+        Query _qur = new Query();
+		_qurser = main.getQueryServerFromID(data.getString("_databaseID")); //Oralce-AReport , Oralce-ALONE29       MySQL-test          
+        _qurser.connectDatabase();
+        
+        //Lay define cho he thong
+        _qur = main.getQueryFromID(data.getString("_defineScript"));
+        _qur.queryToAppDefine(main.get_appcommon(), _qurser);        
+        //main.get_appcommon().logAppCommon();
+        
+		//Tim cron dung        
+        String [] a= null;
+		a = main.getQueryIDFromName(data.getString("_cronNM"));		
+		Query[] _arrQueryTemp = new Query[a.length];		
 		_arrQueryTemp = main.getQueryFromQueryID(a);
 		
 		/*
@@ -69,42 +77,25 @@ public class QueryJob implements Job{
 		
 		//main.logQuery();
 		main.set_query(_arrQueryTemp);
-		
-		//logger.info("\n\n Sau");
-		//main.logQuery();
-		
-		//Chay multi
-		
-		main.sortQueryWithPriority();
-		
-		//logger.info("\n\n Sort lai");
-		//main.logQuery();
-		
-		
-		QueryServer _qurser = new QueryServer();
-        Query _qur = new Query();
-		_qurser = main.getQueryServerFromID("MySQL-test"); //Oralce-AReport , Oralce-ALONE29                
-        _qurser.connectDatabase();
-        
-        /*
-        _qur = main.getQueryFromID("DEF001");
-        _qur.queryToAppDefine(main.get_appcommon(), _qurser);
-        */
-        //a.get_appcommon().logAppCommon();
-        
-        //Tien hanh chay query
+						
+		//Sort lai query theo thu tu uu tien
+		main.sortQueryWithPriority();				
+
         //set cac thong tin cua Query
-        for (int j=0; j<main.get_query().length;j++){
+        for (int j=0; j<main.get_query().length;j++)
+        {        	
+        	main.get_query()[j].set_querynm(main.get_appcommon().get_define().get("01h_trdt")+"_"+main.get_query()[j].get_querynm());
         	main.get_query()[j].set_fileurl(main.get_appcommon().get_scriptUrl()+main.get_query()[j].get_fileurl());
     		//doc file
         	main.get_query()[j].readScript();    	
     		//this.logQuery();
         	main.get_query()[j].set_define(main.get_appcommon().get_define());
         	main.get_query()[j].setquery();        	
-        	//a.get_query()[j].logQuery();
-        }
+        	//main.get_query()[j].logQuery();
+        }        
         
-        
+        //Tien hanh chay multi query        
         RunMulConScript.commandMulQueryExcel(_qurser, main.get_query(), main.get_appcommon());
-	}     
+      		
+	}   
 }
