7e90d3c2b10ba7a62cc2bd3674d54be65b3828d9
tuanemtv.project@gmail.com <tuanemtv.project@gmail.com@a1609d13-3ef6-a8f8-5e0b-472c95e60613>
2012-11-18 13:08:16 +0000

    Complete run all scripts

--- trunk/Query To Excel Cron/src/org/eib/cron/application/views/AllScriptView.java
+++ trunk/Query To Excel Cron/src/org/eib/cron/application/views/AllScriptView.java
@@ -1,16 +1,36 @@
 package org.eib.cron.application.views;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+
 import org.eclipse.ui.part.ViewPart;
+
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Button;
@@ -19,17 +39,31 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eib.common.FolderUtil;
 import org.eib.common.MainCommon;
 import org.eib.common.QueryServer;
+import org.eib.database.CommandMultiQuery;
 import org.eib.database.Query;
-import org.eib.thread.RunMulConScript;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.wb.swt.SWTResourceManager;
 
 public class AllScriptView extends ViewPart {
 
 	public static final String ID = "org.eib.cron.application.views.AllScriptView"; //$NON-NLS-1$
 	public static Combo cboDBA;
 	public static Combo cboDefine;
+	public static Button btnRun;
 	private static Logger logger =Logger.getLogger("AllScriptView");
+	private MainCommon main;
+	private String _serID = "MySQL-test";
+	private QueryServer _qurser;
+	private Query _query;
+	private Text txtSum;
+	private Text txtCnt;
+	private static int numScript = 0;
 	
-	
+	private Date _date1, _date2;
+	private	DateFormat _dateFormat1, _dateFormat2;
+	private Text txtStatus;
+	private Query[] _runQuery;
+		
 	public AllScriptView() {
 	}
 
@@ -48,136 +82,189 @@ public class AllScriptView extends ViewPart {
 		
 		cboDBA = new Combo(container, SWT.NONE);
 		cboDBA.setItems(new String[] {"Oralce-ALONE29", "Oralce-AReport", "Oralce-ALive", "MySQL-test"});
-		cboDBA.setBounds(62, 7, 149, 23);
+		cboDBA.setBounds(62, 7, 119, 23);
 		
 		Label lblDefine = new Label(container, SWT.NONE);
-		lblDefine.setBounds(217, 10, 34, 15);
+		lblDefine.setBounds(10, 39, 34, 15);
 		lblDefine.setText("Define");
 		
 		cboDefine = new Combo(container, SWT.NONE);
 		cboDefine.setItems(new String[] {"DEF002", "DEF001", "DEF003"});
-		cboDefine.setBounds(257, 7, 74, 23);
+		cboDefine.setBounds(62, 36, 74, 23);
 		
-		Button btnAll = new Button(container, SWT.NONE);
-		btnAll.addSelectionListener(new SelectionAdapter() {
+		btnRun = new Button(container, SWT.NONE);
+		btnRun.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				ResourceBundle rbTest = ResourceBundle.getBundle("/resource/app");	
-				MainCommon maintest = new MainCommon("/resource/app",rbTest.getString("app_kind"),"all");
-				maintest.logQueryCron();				
-				maintest.logQueryServer();
-				maintest.logQuery();
-				logger.info("");
-				logger.info("");
-				logger.info("Running All Script to Excel---------------------------------");				
-				
-				String _outZipUrl;
-				String _fileNameZip;
-				
-				ResourceBundle rb = ResourceBundle.getBundle("/resource/app");				
-				MainCommon main = new MainCommon("/resource/app",rb.getString("app_kind"),"all");
-				
-				Date date1 ;
-				DateFormat dateFormat1;
-				DateFormat dateFormat2;
-				
-				dateFormat1 = new SimpleDateFormat("yyyyMMdd");	
-				date1= new Date();
-				//Set lai duong dan out
-				main.get_appcommon().set_outurl(main.get_appcommon().get_outurl()+dateFormat1.format(date1)+"\\");		
-				FolderUtil.createFolder(main.get_appcommon().get_outurl());
-				
-				//set FTP out
-				main.get_appcommon().set_ftpUrl(main.get_appcommon().get_ftpUrl()+ dateFormat1.format(date1)+"\\");
-				
-				_outZipUrl = main.get_appcommon().get_outurl();
-				_fileNameZip = dateFormat1.format(date1);
-				
-				dateFormat2 = new SimpleDateFormat("HHmm");	
-				date1= new Date();
-				main.get_appcommon().set_outurl(main.get_appcommon().get_outurl()+dateFormat2.format(date1)+"\\");	
-				
-				_fileNameZip = _fileNameZip +" - " + dateFormat2.format(date1);
-				
-				//Tao folder
-				FolderUtil.createFolder(main.get_appcommon().get_outurl());				
-													
-				QueryServer _qurser = new QueryServer();
-		        Query _qur = new Query();
-				_qurser = main.getQueryServerFromID(cboDBA.getText()); //Oralce-AReport , Oralce-ALONE29       MySQL-test          
-		        _qurser.connectDatabase();
-		        
-		        //Lay define cho he thong
-		        _qur = main.getQueryFromID(cboDefine.getText()); //DEF002: Ngay he thong, DEF003 test
-		        _qur.queryToAppDefine(main.get_appcommon(), _qurser);        
-		        //main.get_appcommon().logAppCommon();
-		        											
-				//Sort lai query theo thu tu uu tien
-				main.sortQueryWithPriority();				
-
-		        //set cac thong tin cua Query
-		        for (int j=0; j<main.get_query().length;j++)
-		        {        	
-		        	main.get_query()[j].set_querynm(main.get_appcommon().get_define().get("01h_trdt")+"_"+main.get_query()[j].get_querynm());
-		        	main.get_query()[j].set_fileurl(main.get_appcommon().get_scriptUrl()+main.get_query()[j].get_fileurl());
-		    		//doc file
-		        	main.get_query()[j].readScript();    	
-		    		//this.logQuery();
-		        	main.get_query()[j].set_define(main.get_appcommon().get_define());
-		        	main.get_query()[j].setquery();        	
-		        	//main.get_query()[j].logQuery();
-		        }        
-		        
-		        //Tien hanh chay multi query        
-		        RunMulConScript.commandMulQueryExcel(_qurser, main.get_query(), main.get_appcommon());
-		      		
-		        //logger.error("main.get_appcommon().get_outurl()= "+main.get_appcommon().get_outurl());
-		        //logger.info("_outZipUrl= "+_outZipUrl);
-		       // logger.info("_fileNameZip= "+_fileNameZip);
-		        //Tien hanh Zip file
-		        
-		        //ZipUtil.creatZipFoler("D:\\20121109\\2233\\","D:\\20121109\\","20121109 - 2233");
-		        /*
-		        ZipUtil.creatZipFoler(main.get_appcommon().get_outurl(),_outZipUrl,_fileNameZip);
-		        
-		        //Tao foler tren FTP
-		        FTPUtil ftp = new FTPUtil();
-		        ftp.set_ftpServer(main.get_appcommon().get_ftpServer());
-		        ftp.set_user(main.get_appcommon().get_ftpUsr());
-		        ftp.set_password(main.get_appcommon().get_ftpPass());
-		        ftp.set_port(21);
-		               
-		        ftp.createFolder(main.get_appcommon().get_ftpUrl());
-		                
-		        //Dua len FTP
-		        //main.get_appcommon().set_ftpFilename(main.get_appcommon().get_ftpFilename()+_fileNameZip+".zip");
-		        main.get_appcommon().set_ftpFilename(_fileNameZip+".zip");
-		        main.get_appcommon().set_ftpInurl(_outZipUrl+_fileNameZip+".zip");
-		        File source = new File(main.get_appcommon().get_ftpInurl());
-		        
-		        //main.get_appcommon().logAppCommon();
-		        
-		        try {
-					//upload(ftpServer,user,password,fileName,source);
-					FTPUtil.upload(ftp.get_ftpServer(),
-							ftp.get_user(),
-							ftp.get_password(),
-							main.get_appcommon().get_ftpUrl()+main.get_appcommon().get_ftpFilename(),
-							source);
-				} catch (MalformedURLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					logger.error(e.getMessage());		
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					logger.error(e.getMessage());		
-				}*/
-		        
+			
+				logger.info("numScript= "+numScript);
+				if (numScript ==0){ //Chua thuc hien					
+					
+					ResourceBundle rb = ResourceBundle.getBundle("/resource/app");				
+					main = new MainCommon("/resource/app",rb.getString("app_kind"),"all");
+					
+					logger.info("");
+					logger.info("");
+					logger.info("Running All Script to Excel---------------------------------");				
+					
+					String _outZipUrl;
+					String _fileNameZip;				
+					
+					Date date1 ;
+					DateFormat dateFormat1;
+					DateFormat dateFormat2;
+					
+					dateFormat1 = new SimpleDateFormat("yyyyMMdd");	
+					date1= new Date();
+					//Set lai duong dan out
+					main.get_appcommon().set_outurl(main.get_appcommon().get_outurl()+dateFormat1.format(date1)+"\\");		
+					FolderUtil.createFolder(main.get_appcommon().get_outurl());
+					
+					//set FTP out
+					main.get_appcommon().set_ftpUrl(main.get_appcommon().get_ftpUrl()+ dateFormat1.format(date1)+"\\");
+					
+					_outZipUrl = main.get_appcommon().get_outurl();
+					_fileNameZip = dateFormat1.format(date1);
+					
+					dateFormat2 = new SimpleDateFormat("HHmm");	
+					date1= new Date();
+					main.get_appcommon().set_outurl(main.get_appcommon().get_outurl()+dateFormat2.format(date1)+"\\");	
+					
+					_fileNameZip = _fileNameZip +" - " + dateFormat2.format(date1);
+					
+					//Tao folder
+					FolderUtil.createFolder(main.get_appcommon().get_outurl());				
+					
+	
+					QueryServer _qurser = new QueryServer();
+			        Query _qur = new Query();
+					_qurser = main.getQueryServerFromID(cboDBA.getText()); //Oralce-AReport , Oralce-ALONE29       MySQL-test          
+			        _qurser.connectDatabase();
+			        
+					
+			        //Lay define cho he thong
+			        _qur = main.getQueryFromID(cboDefine.getText()); //DEF002: Ngay he thong, DEF003 test
+			        _qur.queryToAppDefine(main.get_appcommon(), _qurser);        
+			        //main.get_appcommon().logAppCommon();
+			        											
+					//Sort lai query theo thu tu uu tien
+					main.sortQueryWithPriority();				
+	
+			        //set cac thong tin cua Query
+			        for (int j=0; j<main.get_query().length;j++)
+			        {        	
+			        	main.get_query()[j].set_querynm(main.get_appcommon().get_define().get("01h_trdt")+"_"+main.get_query()[j].get_querynm());
+			        	main.get_query()[j].set_fileurl(main.get_appcommon().get_scriptUrl()+main.get_query()[j].get_fileurl());
+			    		//doc file
+			        	main.get_query()[j].readScript();    	
+			    		//this.logQuery();
+			        	main.get_query()[j].set_define(main.get_appcommon().get_define());
+			        	main.get_query()[j].setquery();        	
+			        	//main.get_query()[j].logQuery();
+			        }        
+			        
+			       // main.logQuery();
+			        
+			        //Thoi gian bat dau			        
+			       	_dateFormat1 = new SimpleDateFormat("yyyyMMdd_HHmmss");	        	
+		    		_date1= new Date();
+		    			    							
+					//Cau query chay
+		    		_runQuery = main.getRunQuery(main.get_query());
+					
+					//Set thong tin				
+					txtSum.setText(String.valueOf(_runQuery.length));
+					txtCnt.setText(String.valueOf(main.get_appcommon().get_scriptnums()));
+					
+					//btnRun.setEnabled(false);
+					cboDBA.setEnabled(false);
+					cboDefine.setEnabled(false);
+					
+					txtStatus.setText("Run");
+					btnRun.setText("Run Back");
+					
+					//Chay
+					runAllScript();
+					
+					Job job = new Job("Run All Scripts") {
+						@Override
+						protected IStatus run(IProgressMonitor monitor) {
+							// Set total number of work units
+							monitor.beginTask("Total script: "+(_runQuery.length), _runQuery.length);
+							//for (int i = 0; i < main.get_query().length; i++) {
+							
+							monitor.worked(_runQuery.length/2);
+							// Sleep a second
+							//TimeUnit.SECONDS.sleep(1);	
+							//while((numScript + 1) != main.get_query().length){
+							while (main.chekFinishQuery(_runQuery) != 1){
+								//logger.info("numScript = "+numScript);
+								monitor.subTask(" Run scriptID: " + numScript+ " with name= "+_runQuery[numScript].get_querynm());				     
+								// Report that 20 units are done
+								//if (numScript>2)
+									//monitor.worked(numScript);									
+								//monitor.worked(20);
+								//txtCnt.setText(String.valueOf(numScript));
+							}
+							
+							//Doi 2s
+							try {
+								Thread.sleep(3000);//3s
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							
+							showResult();
+							monitor.done();
+							return Status.OK_STATUS;
+					  }
+					 };
+					job.schedule();
+					
+				}else{
+					if ((numScript + 1) == _runQuery.length){
+						//Da chay xong
+						numScript = 0;
+						txtStatus.setText("Running back");
+						cboDBA.setEnabled(true);
+						cboDefine.setEnabled(true);
+						btnRun.setText("Run");
+						txtSum.setText("");
+						txtCnt.setText("");
+					}else{
+						txtStatus.setText("Running");
+					}
+				}
+														        
+		        //btnRun.setEnabled(true);
 			}
 		});
-		btnAll.setBounds(337, 5, 103, 25);
-		btnAll.setText("Run All Script");
+		
+		btnRun.setBounds(10, 66, 75, 25);
+		btnRun.setText("Run");
+		
+		txtSum = new Text(container, SWT.BORDER);
+		txtSum.setForeground(SWTResourceManager.getColor(SWT.COLOR_DARK_BLUE));
+		txtSum.setEnabled(false);
+		txtSum.setBounds(88, 94, 57, 21);
+		
+		txtCnt = new Text(container, SWT.BORDER);
+		txtCnt.setEnabled(false);
+		txtCnt.setForeground(SWTResourceManager.getColor(SWT.COLOR_DARK_BLUE));
+		txtCnt.setBounds(88, 121, 57, 21);
+		
+		Label lblNewLabel = new Label(container, SWT.NONE);
+		lblNewLabel.setBounds(10, 97, 72, 15);
+		lblNewLabel.setText("Total Scripts");
+		
+		Label lblNewLabel_1 = new Label(container, SWT.NONE);
+		lblNewLabel_1.setBounds(10, 124, 66, 15);
+		lblNewLabel_1.setText("Run Scripts");
+		
+		txtStatus = new Text(container, SWT.BORDER);
+		txtStatus.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
+		txtStatus.setBounds(10, 148, 157, 21);
 
 		createActions();
 		initializeToolBar();
@@ -191,6 +278,7 @@ public class AllScriptView extends ViewPart {
 		// Create the actions
 		cboDBA.setText("Oralce-ALONE29");
 		cboDefine.setText("DEF002");
+				
 	}
 
 	/**
@@ -212,5 +300,88 @@ public class AllScriptView extends ViewPart {
 	@Override
 	public void setFocus() {
 		// Set the focus
+	}
+	
+	
+	
+	public void runScript(){		
+		CommandMultiQuery cq1 = new CommandMultiQuery(_qurser.get_conn(),_query,main.get_appcommon());
+		cq1.run();
+	}
+	
+	
+	public void runAllScript(){
+		
+		Runnable limitedCall = new Runnable() {		
+            final Semaphore available = new Semaphore(main.get_appcommon().get_scriptnums());
+            int count = 0;	
+            
+            public void run()
+            {               
+                int num = count++; //Dung cho mang		                
+                try
+                {
+                    available.acquire();                    
+                    for (int l=0; l<_runQuery.length;l++){
+                    	if (l == num){
+                    	   numScript = num;
+                    		  
+                    	  _qurser = new QueryServer();						
+ 		            	  _qurser = main.getQueryServerFromID(_serID);
+ 		            	  _qurser.connectDatabase();
+ 		            	  
+ 		            	  _query = new Query();
+ 		            	  _query = _runQuery[l];
+ 		            	   
+ 		            	   logger.info(">> Run " + num+" = "+_runQuery[l].get_querynm());
+ 		            	   //txtCnt.setText(String.valueOf(num + 1));
+ 		            	   runScript();
+ 		            	  		 		            	  
+                    	}
+                    }		                                        
+                    available.release();
+                }
+                catch (InterruptedException intEx)
+                {
+                    intEx.printStackTrace();
+                }
+            }		            		           
+        };
+        
+        for (int i=0; i<_runQuery.length; i++)
+            new Thread(limitedCall).start();
+	}
+	
+	public void showResult(){
+		//Thong ke thoi gian chay
+		//btnRun.setEnabled(true);
+		
+		//Chay lai neu bi Fail		
+		//logger.info("So luong script bi fail: "+coutRunFailQuery(main.get_query()));
+		
+		_dateFormat2 = new SimpleDateFormat("yyyyMMdd_HHmmss");	
+        _date2= new Date();
+        
+        logger.info("");
+        logger.info("Thoi gian chay -------------------");
+        logger.info("Bat dau : " +_dateFormat1.format(_date1));
+        logger.info("Ket thuc: " +_dateFormat2.format(_date2));
+        logger.info("Tong tg : " + String.valueOf(Math.abs(_date2.getTime() - _date1.getTime())/1000)+"s");
+        //Thong tin
+        logger.info("");
+        logger.info("Chi tiet Thoi gian -------------------");
+        main.logQuerySumReport(_runQuery);
+        main.logQuerySumReport(_runQuery,"DP");
+        main.logQuerySumReport(_runQuery,"LN");
+        main.logQuerySumReport(_runQuery,"GL");
+        main.logQuerySumReport(_runQuery,"TF");
+        main.logQuerySumReport(_runQuery,"DL");
+        main.logQuerySumReport(_runQuery,"EI");
+        main.logQuerySumReport(_runQuery,"CS");
+        logger.info("");
+        logger.info("Chi tiet Script -------------------");
+        main.logShowScript(_runQuery);               
+        //kiem tra script fail de chay lai
+        
 	}
 }
