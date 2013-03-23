4f555eff08b98e0f92244be995b4a49c0a9652e4
tuanemtv.gogo <tuanemtv.gogo@a1609d13-3ef6-a8f8-5e0b-472c95e60613>
2013-03-23 16:43:19 +0000

    Test Mail

---
+++ org.eib.application.ui/test/test/org/eib/mail/TestMailUtil.java
@@ -0,0 +1,31 @@
+package test.org.eib.mail;
+
+import org.eib.common.MailUtil;
+
+public class TestMailUtil {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MailUtil mail = new MailUtil();
+		mail.set_smtpServer("smtp.gmail.com");
+		mail.set_toMail("tuanemtv@gmail.com");
+		mail.set_frMail("tuanemtv.gogo@gmail.com");
+		mail.set_passFrMail("tuan1985em");
+		mail.set_subject("Tieu de mail");
+		mail.set_bodyKind("1");
+		mail.set_ContMail("<h2>Noi dung mail</h2>");
+		mail.set_fileUrl("D:\\test.txt");
+		mail.set_fileName("test1.txt");
+		try {
+			mail.sendMail();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
